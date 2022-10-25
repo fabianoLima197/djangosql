9f1d6ccbcc5fb3b30b1fbe1bd376bf44c275bba7
Fabiano <fabianolimaads@gmail.com>
2022-10-25 17:49:37 -0300

    vamos vendo o contato

--- core/models.py
+++ core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from stdimage.models import StdImageField
+
+#SIGNALS
+from django.db.models import signals
+from django.template.defaultfilters import slugify
 
 # Create your models here.
+
+class base(models.Model):
+    criado = models.DateField('Dada de Criação', auto_now=True)
+    modificado = models.DateField('Data de atualização', auto_now=True)
+    ativo = models.DateField('Ativo', default=True)
+
+    class Meta:
+        abstract = True
+
+class Produto(Base):
+    nome = models.CharField('Nome', max_length=100)
+    preco = models.DecimalField('Preço', max_digits=8, decimal_places=2)
+    estoque = models.IntegerField('Estoque')
+    imagem = StdImageField('Imagem', upload_to='produtos', variations={'thumb': (124, 124)})
+    slug = models.SlugField('Slug', max_length=100, blank=True, editable=False)
+
+    def __str__(self):
+        return self.nome
