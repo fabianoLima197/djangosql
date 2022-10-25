74bf15499f6177b80f67d3cf28d3297fdbc83b30
Fabiano <fabianolimaads@gmail.com>
2022-10-25 15:04:33 -0300

    vamos vendo o contato

--- core/views.py
+++ core/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-
+from .forms import ContatoForm
 # Create your views here.
+
+
+
 def index(reguest):
     return render(reguest, 'index.html')
 
 def contato(request):
-    return render(request, 'contato.html')
+    form = ContatoForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'contato.html', context)
 
 def produto(request):
     return render(request, 'produto.html')
